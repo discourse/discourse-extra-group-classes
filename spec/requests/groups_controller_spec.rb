14a0a44652a44601e68080381e7df9befe2c90fd
Jeff Wong <awole20@gmail.com>
2021-04-20 10:37:53 -0700

    update tests

--- spec/requests/groups_controller_spec.rb
+++ spec/requests/groups_controller_spec.rb
@@ -10,25 +10,41 @@ describe "Groups Controller" do
 
   it "should update when updated as an admin" do
     sign_in(admin)
-
     put "/admin/groups/#{group.id}/extra_classes", params: {
           classes: "a|b|c"
         }
 
     expect(response.status).to eq(200)
-
     expect(group.reload.custom_fields["extra_group_classes"]).to eq("a|b|c")
+  end
+
+  it "should error when the class list is too long" do
+    sign_in(admin)
+    put "/admin/groups/#{group.id}/extra_classes", params: {
+          classes: "something-with-wayyyyyy-too-many-words-here"
+        }
+    expect(response.status).to eq(400)
+
+    put "/admin/groups/#{group.id}/extra_classes", params: {
+          classes: "areallyreallyreallylongwordherethisistoolongwaywaytoolong"
+        }
+    expect(response.status).to eq(400)
+
+    put "/admin/groups/#{group.id}/extra_classes", params: {
+          classes: "way|too|may|classes|here|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s"
+        }
+    expect(response.status).to eq(400)
+
+    expect(group.reload.custom_fields["extra_group_classes"]).to be_nil
   end
 
   it "should 404 for a normal user" do
     sign_in(user)
-
     put "/admin/groups/#{group.id}/extra_classes", params: {
           classes: "a|b|c"
         }
 
     expect(response.status).to eq(404)
-
     expect(group.reload.custom_fields["extra_group_classes"]).to be_nil
   end
 
