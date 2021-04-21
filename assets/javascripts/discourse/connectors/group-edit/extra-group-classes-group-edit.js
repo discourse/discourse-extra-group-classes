582233fd17fff451a514d4d80047a68c6f0d458b
Jeff Wong <awole20@gmail.com>
2021-04-20 18:32:29 -0700

    update front end, use a list setting to set components

--- assets/javascripts/discourse/connectors/group-edit/extra-group-classes-group-edit.js
+++ assets/javascripts/discourse/connectors/group-edit/extra-group-classes-group-edit.js
@@ -1,9 +1,17 @@
 import { ajax } from "discourse/lib/ajax";
+import { computed } from "@ember/object";
 
 export default {
+  tokenSeparator: "|",
+
+  extraClasses: computed("group.extra_classes", function () {
+    return this.model.extra_classes.split(this.tokenSeparator).filter(Boolean);
+  }),
+
   actions: {
     extraGroupClassesChanged(value) {
-      group.set("extra_classes", value);
+      newValue = value.join(this.tokenSeparator);
+      group.set("extra_classes", newValue);
 
       return ajax(`/admin/groups/${group.id}/extra_classes`, {
         type: "PUT",
