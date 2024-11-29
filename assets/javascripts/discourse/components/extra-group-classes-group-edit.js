0b410b041beaa21d39621ae862cfdc8ec34cd0b8
David Taylor <david@taylorhq.com>
2024-11-29 15:41:49 +0000

    DEV: Convert to native class syntax (#20)

--- assets/javascripts/discourse/components/extra-group-classes-group-edit.js
+++ assets/javascripts/discourse/components/extra-group-classes-group-edit.js
@@ -1,41 +1,42 @@
 import Component from "@ember/component";
-import { computed } from "@ember/object";
+import { action, computed } from "@ember/object";
 import { service } from "@ember/service";
 import { ajax } from "discourse/lib/ajax";
 import I18n from "discourse-i18n";
 
-export default Component.extend({
-  dialog: service(),
-  tokenSeparator: "|",
+export default class ExtraGroupClassesGroupEdit extends Component {
+  @service dialog;
 
-  extraClasses: computed("group.extra_classes", function () {
+  tokenSeparator = "|";
+
+  @computed("group.extra_classes")
+  get extraClasses() {
     if (this.group.extra_classes) {
       return this.group.extra_classes
         .split(this.tokenSeparator)
         .filter(Boolean);
     }
     return null;
-  }),
+  }
 
-  actions: {
-    extraGroupClassesChanged(value) {
-      let newValue = value.join(this.tokenSeparator);
-      let group = this.group;
-      let oldValue = this.group.extra_classes;
+  @action
+  extraGroupClassesChanged(value) {
+    let newValue = value.join(this.tokenSeparator);
+    let group = this.group;
+    let oldValue = this.group.extra_classes;
 
-      group.set("extra_classes", newValue);
+    group.set("extra_classes", newValue);
 
-      return ajax(`/admin/groups/${group.id}/extra_classes`, {
-        type: "PUT",
-        data: group.getProperties("extra_classes"),
-      }).catch(() => {
-        group.set("extra_classes", oldValue);
-        this.dialog.alert(
-          I18n.t("generic_error_with_reason", {
-            error: I18n.t("extra_group_classes.save_error"),
-          })
-        );
-      });
-    },
-  },
-});
+    return ajax(`/admin/groups/${group.id}/extra_classes`, {
+      type: "PUT",
+      data: group.getProperties("extra_classes"),
+    }).catch(() => {
+      group.set("extra_classes", oldValue);
+      this.dialog.alert(
+        I18n.t("generic_error_with_reason", {
+          error: I18n.t("extra_group_classes.save_error"),
+        })
+      );
+    });
+  }
+}
